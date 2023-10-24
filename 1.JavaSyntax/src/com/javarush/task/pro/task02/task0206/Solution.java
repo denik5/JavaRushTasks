392ed21f81e889964ae9ed30a341c87538e589c3
denik5 <white90@inbox.ru>
2023-10-24 17:59:45 +0300

    Init

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Home", phrase = "Java", line = "River", text = "Luck";
+
+    }
+}
