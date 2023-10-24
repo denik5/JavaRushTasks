f5a5701c9e32799b665c904f13b5fd8bc4f9ef51
denik5 <white90@inbox.ru>
2023-10-24 14:21:22 +0300

    Init

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int year = 3126;
+        int bornago = 8;
+        int agefriend = year - bornago;
+        System.out.println(agefriend);
+    }
+}
