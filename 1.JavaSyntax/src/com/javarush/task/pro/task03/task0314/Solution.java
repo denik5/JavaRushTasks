5ccee28ec44eae40e1e8adda595ec7bea3a9614d
denik5 <white90@inbox.ru>
2023-10-27 11:15:11 +0300

    Init

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+
+        if(str1.equalsIgnoreCase(secret)) System.out.println("доступ разрешен");
+        else System.out.println("доступ запрещен");
+    }
+}
