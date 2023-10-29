d574fee6e834c151cc4bd3dc01aef9cbe5b68f9b
denik5 <white90@inbox.ru>
2023-10-29 14:44:55 +0300

    add solution JavaSyntax task0418

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0418/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task04.task0418;
+
+import java.util.Scanner;
+
+/* 
+Стакан наполовину пуст или наполовину полон?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double glass = 0.5;
+
+        Scanner sc = new Scanner(System.in);
+        boolean flag = sc.nextBoolean();
+
+
+        if(flag == true) {
+            System.out.println((int)Math.ceil(glass));
+        } else {
+            System.out.println((int)Math.floor(glass));
+        }
+    }
+}
