5fb05568603847ccccc4b591f6f67f4db7a4c6b6
denik5 <white90@inbox.ru>
2023-11-03 21:24:56 +0300

    add solution JavaSyntax task0504

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0519;
+
+import java.util.Arrays;
+
+/* 
+Есть ли кто?
+*/
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        int[] copy = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i];
+        }
+        Arrays.sort(copy);
+        int index = Arrays.binarySearch(copy, element);
+        System.out.println(index >=0 ? true : false);
+    }
+}
