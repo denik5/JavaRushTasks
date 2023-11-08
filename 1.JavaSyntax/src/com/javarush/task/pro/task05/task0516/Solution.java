4ee188e7242418b050e1b18c7a4679f9320c8d5b
denik5 <white90@inbox.ru>
2023-11-08 20:43:47 +0300

    add solution JavaSyntax task0516

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task05.task0516;
+
+import java.util.Arrays;
+
+/* 
+Заполняем массив
+*/
+
+public class Solution {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        int i = array.length;
+        int h = (array.length/2) + ((array.length)%2);
+
+        Arrays.fill(array, 0, h, valueStart);
+        Arrays.fill(array, h, i, valueEnd);
+
+        System.out.println(Arrays.toString(array));
+
+    }
+}
