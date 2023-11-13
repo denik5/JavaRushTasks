aed69484308497b8a3df2c4d34c2837e32a8ca5f
denik5 <white90@inbox.ru>
2023-11-13 21:49:56 +0300

    add solution JavaSyntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a;
+    long b;
+    float c;
+    double d;
+    char l;
+    boolean flag;
+    short g;
+    byte t;
+}
