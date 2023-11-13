440f16abbb10ccc257a085564bae71fbe2ed0e3d
denik5 <white90@inbox.ru>
2023-11-13 22:21:17 +0300

    add solution JavaSyntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+   byte a = -128;
+   short b = -32768;
+   int c = -2147483648;
+   long d = -9223372036854775808L;
+   float f = -100000000000F;
+   double g = -100000000000D;
+   boolean j = false;
+   char k = 0;
+}
