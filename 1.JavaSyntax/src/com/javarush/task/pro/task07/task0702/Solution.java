ca29618919603b3f799317f4ec3f26b474452ab5
denik5 <white90@inbox.ru>
2023-11-13 22:17:32 +0300

    add solution JavaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte a = 127;
+    short b = 32767;
+    int c = 2147483647;
+    long d  =  9223372036854775807L;
+    float g = 3.400000000000000000000000000000000000000F;
+    double h = 9.9000000000000000000000000000000000000D;
+    boolean flag = true;
+    char j = 65535;
+}
