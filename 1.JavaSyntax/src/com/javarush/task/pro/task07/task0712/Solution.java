a1b4dd24081998428bd338058ac08e871771a225
denik5 <white90@inbox.ru>
2023-11-17 22:34:58 +0300

    add solution JavaSyntax task0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String();
+        String b = new String();
+        String c = new String();
+        String d = new String();
+        String e = new String();
+        String f = new String();
+        String g = new String();
+        String h = new String();
+        String j = new String();
+        String ja = new String();
+        int[] ab = new int[5];
+        int[] ac = new int[5];
+        int[] ag = new int[5];
+        int[] ah = new int[5];
+        int[] at = new int[5];
+        Scanner sc = new Scanner(System.in);
+        int rw = sc.nextInt();
+        Scanner sc2 = new Scanner(System.in);
+        int rh = sc2.nextInt();
+    }
+}
