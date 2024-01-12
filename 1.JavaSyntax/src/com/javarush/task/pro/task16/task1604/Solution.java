f527fa2fa0ef49bcf755c70439f1cf017a8ee26e
denik5 <white90@inbox.ru>
2024-01-12 20:09:15 +0300

    add solution JavaSyntax task1604

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(92, Calendar.OCTOBER, 19);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+    static String getDayOfWeek(Calendar calendar) {
+            String result = "";
+            int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case 1 -> {
+                return "Воскресенье";
+            }
+            case 2 -> {
+                return "Понедельник";
+            }
+            case 3 -> {
+                return "Вторник";
+            }
+            case 4 -> {
+                return "Среда";
+            }
+            case 5 -> {
+                return "Четверг";
+            }
+            case 6 -> {
+                return "Пятница";
+            }
+            case 7 -> {
+                return "Суббота";
+            }
+        }
+        return result;
+    }
+}
