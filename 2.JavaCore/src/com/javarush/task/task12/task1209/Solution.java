55e2b2f12fe59eaf16f699bd87ffcff48a0df6bd
denik5 <white90@inbox.ru>
2024-02-01 22:33:10 +0300

    add solution JavaCore task1209

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int min, int max) {
+        return Math.min(max, min);
+    }
+    public static long min(long min, long max) {
+        return Math.min(max, min);
+    }
+    public static double min(double min, double max) {
+        return Math.min(max, min);
+    }
+}
