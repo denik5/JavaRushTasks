9115fdcb11aeb8c2b29f649d42e269ec4733009a
denik5 <white90@inbox.ru>
2024-02-04 13:28:19 +0300

    add solution JavaCore task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a, int b) {
+
+    }
+
+    public static void print(String str) {
+
+    }
+
+    public static void print(double d) {
+
+    }
+
+    public static void print(int ad) {
+
+    }
+
+    public static void print(String a, int b) {}
+
+
+}
