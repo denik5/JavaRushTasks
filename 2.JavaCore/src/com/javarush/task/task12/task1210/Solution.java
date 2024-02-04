82eecf8355be2ad842404919218a166321869119
denik5 <white90@inbox.ru>
2024-02-04 12:22:24 +0300

    add solution JavaCore task1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int n1, int n2) {
+        return Math.max(n1, n2);
+    }
+
+    public static long max(long l1, long l2) {
+        return Math.max(l1, l2);
+    }
+
+    public static double max(double d1, double d2) {
+        return Math.max(d1, d2);
+    }
+}
